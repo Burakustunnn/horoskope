4b3c4e98c4639c771d2faf78b5c9b9a6a603e51c
Burak Ustun <burakustun54@hotmail.com>
2023-02-16 21:18:55 +0300

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.scss"
+
+
+const Header =()=>{
+return(
+    <div className="header">
+        <span className="test-shadow" >Who are you ?</span>
+    </div>
+
+
+
+
+)
+}
+export default Header;
